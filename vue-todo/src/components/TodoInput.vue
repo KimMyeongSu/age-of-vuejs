9a47b4db588c4556f5de909ddae1785d3a89e45c
KimMyeongSu <Myeongsu@likelion.org>
2022-10-16 20:36:44 +0900

    Vue TodoList 예제 추가(Create) 기본컴포넌트 생성

---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input type="text" v-model="newTodoItem">
+    <button v-on:click="addButton">add</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return {
+            newTodoItem:""
+        }
+    },
+    methods:{
+    addButton: function(){
+        localStorage.setItem(this.newTodoItem,this.newTodoItem);
+        this.clearInput
+    },
+    clearInput: function(){
+        this.newTodoItem="";
+    }
+
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-todo/vue.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+
+"eslint.workingDirectories": [
+
+  {"mode": "auto"}
+  
+  ],
