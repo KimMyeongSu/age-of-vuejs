b85c835577d9bdf3cc0d749ec9b79efc728372ea
KimMyeongSu <Myeongsu@likelion.org>
2022-11-24 15:02:28 +0000

    vuex 수정중

--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
-
+    // counter라는 headerText 속성을 추가
+    state: {
+        headerText: 'TODO it!',
+    }
 });
